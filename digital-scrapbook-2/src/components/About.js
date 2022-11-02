6be4557c1449b88ba4e944a80911f6aa65029e2b
Andrew McLean <andrew.mclean.5895@gmail.com>
2022-11-02 16:46:00 -0600

    Final

--- digital-scrapbook-2/src/components/About.js
+++ digital-scrapbook-2/src/components/About.js
@@ -3,12 +3,24 @@ import React from "react";
 function About(){
     return(
         <div className="about">
-            <h1>Digital Concert Scrapbook</h1>
-            <p> 
-                As a lover of all things Grateful Dead, I go to a lot of shows. 
-                This website allows me to keep track of the shows and the 
-                respective set-lists. 
-            </p>
+            <h1 >Digital Concert Scrapbook</h1>
+            <ul className="description"> 
+
+                <h4>You can...</h4>
+    
+                <li> Add a concert with: band name, image, date, location, venue , and set-list.</li>
+                <br></br>
+                
+                <li>Delete a show from the list. </li>
+                <br></br>
+                <li>Learn more about the show by clicking on the View Set-list button. </li>
+                <br></br>
+                <li>Search by song name, and the shows in which that song was played will render. </li>
+                <br></br>
+                <li>Click through to see more pictures, one picture will be the show poster, the next will be a picture  from the show. </li>
+                <br></br>
+                <li>See video footage of some of the shows on the cards.  </li>
+            </ul>
         </div>
     )
 }
